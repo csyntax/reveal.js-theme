25284359d5b427986b9bfab999f5f3a7a7f35060
csyntax <csyntax@outlook.com>
2016-11-04 14:53:41 +0200

    update plugins paths

--- scripts/render.js
+++ scripts/render.js
@@ -14,46 +14,46 @@ function render(filename) {
 				progress: true,
 				history: true,
 				center: true,
-				slideNumber: 'c / t',
-				transition: 'slide',
+				slideNumber: "c / t",
+				transition: "slide",
 				dependencies: [
 		            {
-		                src: './reveal.js/lib/js/classList.js',
-		                condition: function() {
+		                src: "http://rawgit.com/csyntax/reveal.js-theme/master/reveal.js/lib/js/classList.js",
+		                condition: function () {
 		                    return !document.body.classList;
 		                }
 		            },
 		            {
-		                src: './reveal.js/plugin/markdown/marked.js',
-		                condition: function() {
-		                    return !!document.querySelector('[data-markdown]');
+		                src: "https://raw.githubusercontent.com/csyntax/reveal.js-theme/master/reveal.js/plugin/markdown/marked.js",
+		                condition: function () {
+		                    return !!document.querySelector("[data-markdown]");
 		                }
 		            },
 		            {
-		                src: './reveal.js/plugin/markdown/markdown.js',
-		                condition: function() {
-		                    return !!document.querySelector('[data-markdown]');
+		                src: "https://raw.githubusercontent.com/csyntax/reveal.js-theme/master/reveal.js/plugin/markdown/markdown.js",
+		                condition: function () {
+		                    return !!document.querySelector("[data-markdown]");
 		                }
 		            },
 		            {
-		                src: './reveal.js/plugin/highlight/highlight.js',
-		                //async: true,
+		                src: "https://raw.githubusercontent.com/csyntax/reveal.js-theme/master/reveal.js/plugin/highlight/highlight.js",
+		                async: true,
 		                callback: function() {
 		                    hljs.initHighlightingOnLoad();
 		                }
 		            },
 		            {
-		                src: './reveal.js/plugin/zoom-js/zoom.js',
-		                //async: true
+		                src: "https://raw.githubusercontent.com/csyntax/reveal.js-theme/master/reveal.js/plugin/zoom-js/zoom.js",
+		                async: true
 		            },
 		            {
-		                src: './reveal.js/plugin/notes/notes.js',
-		                //async: true
+		                src: "https://raw.githubusercontent.com/csyntax/reveal.js-theme/master/reveal.js/plugin/notes/notes.js",
+		                async: true
 		            }
 		        ]
 			});
 
-			setTimeout(function() {
+			setTimeout(function () {
 		  		$('#loading').remove();
 			}, 3000);
 	  	},
