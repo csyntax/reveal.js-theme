ebd6ebc37d6c564012cf01f1e1c25ef10d1c5093
csyntax <csyntax@outlook.com>
2016-11-07 10:46:46 +0200

    migrate from power point

---
+++ migration-from-power-point/gulpfile.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var gulp = require("gulp");
+var less = require("gulp-less");
+var watch = require("gulp-watch");
+var server = require("gulp-server-livereload");
+
+gulp.task("less", function () {
+	return gulp.src("./less/**/*.less")
+		.pipe(less({
+			paths: [
+				path.join(__dirname, "less", "includes")
+			]
+		}))
+		.pipe(gulp.dest("./css"))
+});
+
+gulp.task("watch", function () {
+	gulp.watch("./less/**/*.less", ["less"]);
+});
+
+gulp.task("webserver", function() {
+	gulp.src("./")
+		.pipe(server({
+      		livereload: true,
+      		directoryListing: true,
+      		open: true
+    	}));
+});
+
+gulp.task("default", ["less", "watch", "webserver"]);
