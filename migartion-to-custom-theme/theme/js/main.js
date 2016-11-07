a8f29adb2feb2bc88befa8aa478cf3d60c0b808b
csyntax <csyntax@outlook.com>
2016-11-07 20:45:39 +0200

    update render scripts

--- migartion-to-custom-theme/theme/js/main.js
+++ migartion-to-custom-theme/theme/js/main.js
@@ -1,21 +1,41 @@
 $(function() {
+    /*$(".reveal").prepend(`
+        <div id="loading">
+            <div class="loading-container">
+                <div>
+                    <p class="text">Loading</p>
+                </div>
+            </div>
+        </div>
+    `);*/
+
+   /* $(".reveal").append(`
+        <div class="social-widget left desk">
+            <strong>Follow me</strong>
+            <br/>
+            <a href="https://www.facebook.com/csyntaxbg">
+                <img src="../images/facebook.png" />
+            </a>
+        </div>
+    `);*/
+
     var loadingElement = document.createElement("div");
-       loadingElement.id = "loading";
+		loadingElement.id = "loading";
 
-    var loadingContainer = document.createElement("div");
-       loadingContainer.className = "loading-container";
+	var loadingContainer = document.createElement("div");
+		loadingContainer.className = "loading-container";
 
-    var divElement = document.createElement("div");
+	var divElement = document.createElement("div");
 
-    var text = document.createElement("p");
-       text.className = "text";
-       text.innerHTML = "Loading..."
+	var text = document.createElement("p");
+		text.className = "text";
+		text.innerHTML = "Loading..."
 
-       divElement.appendChild(text);
+		divElement.appendChild(text);
 
-       loadingContainer.appendChild(divElement);
+		loadingContainer.appendChild(divElement);
 
-       loadingElement.appendChild(loadingContainer);
+		loadingElement.appendChild(loadingContainer);
 
-    $(".reveal").prepend(loadingElement);
+	$(".reveal").prepend(loadingElement);
 });
